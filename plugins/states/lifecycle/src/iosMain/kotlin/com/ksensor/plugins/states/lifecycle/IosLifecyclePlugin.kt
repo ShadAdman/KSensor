e19a375664b42f3257494daf8010b08ffabceab0
shadmanadman <adman.shadman@gmail.com>
2026-06-21 10:10:43 +0100

    Impl lifecycle

--- plugins/states/lifecycle/src/iosMain/kotlin/com/ksensor/plugins/states/lifecycle/IosLifecyclePlugin.kt
+++ plugins/states/lifecycle/src/iosMain/kotlin/com/ksensor/plugins/states/lifecycle/IosLifecyclePlugin.kt
@@ -3,8 +3,13 @@ package com.ksensor.plugins.states.lifecycle
 import com.ksensor.core.Permission
 import com.ksensor.core.StatePlugin
 import com.ksensor.core.model.StateData
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.callbackFlow
+import platform.Foundation.NSNotificationCenter
+import platform.Foundation.NSOperationQueue
+import platform.UIKit.UIApplicationDidEnterBackgroundNotification
+import platform.UIKit.UIApplicationWillEnterForegroundNotification
 
 class IosLifecyclePlugin : LifecyclePlugin {
     override val id: String = "ksensor.states.lifecycle"
@@ -14,7 +19,25 @@ class IosLifecyclePlugin : LifecyclePlugin {
         override val id: String = "${this@IosLifecyclePlugin.id}.visibility"
         override val requiredPermissions: List<Permission> = emptyList()
         override val currentState: StateData.AppVisibilityStatus = StateData.AppVisibilityStatus(true)
-        override fun observe(): Flow<StateData.AppVisibilityStatus> = emptyFlow()
+
+        override fun observe(): Flow<StateData.AppVisibilityStatus> = callbackFlow {
+            val foregroundObserver = NSNotificationCenter.defaultCenter.addObserverForName(
+                name = UIApplicationWillEnterForegroundNotification,
+                `object` = null,
+                queue = NSOperationQueue.mainQueue
+            ) { trySend(StateData.AppVisibilityStatus(true)) }
+
+            val backgroundObserver = NSNotificationCenter.defaultCenter.addObserverForName(
+                name = UIApplicationDidEnterBackgroundNotification,
+                `object` = null,
+                queue = NSOperationQueue.mainQueue
+            ) { trySend(StateData.AppVisibilityStatus(false)) }
+
+            awaitClose {
+                NSNotificationCenter.defaultCenter.removeObserver(foregroundObserver)
+                NSNotificationCenter.defaultCenter.removeObserver(backgroundObserver)
+            }
+        }
     }
 }
 
