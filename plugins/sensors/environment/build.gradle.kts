4a78f1b0e752c931be601043abd1e83f1df612d5
shadmanadman <adman.shadman@gmail.com>
2026-06-23 13:04:38 +0100

    Fix tests

--- plugins/sensors/environment/build.gradle.kts
+++ plugins/sensors/environment/build.gradle.kts
@@ -43,9 +43,13 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
-                implementation(libs.kotlin.test)
                 implementation(projects.core)
                 implementation(libs.kotlinx.coroutines.core)
+            }
+        }
+        commonTest {
+            dependencies {
+                implementation(libs.kotlin.test)
             }
         }
         getByName("androidDeviceTest") {
