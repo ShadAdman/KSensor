5eea12208144814154b01a468c1ecee259427fa2
shadmanadman <adman.shadman@gmail.com>
2026-06-23 11:35:13 +0100

    UnComment nskey

--- plugins/states/system/src/iosMain/kotlin/com/ksensor/plugins/states/system/VolumeReceiver.kt
+++ plugins/states/system/src/iosMain/kotlin/com/ksensor/plugins/states/system/VolumeReceiver.kt
@@ -11,27 +11,27 @@ import platform.Foundation.NSKeyValueObservingOptionNew
 import platform.Foundation.addObserver
 import platform.Foundation.removeObserver
 import platform.darwin.NSObject
-//import platform.Foundation.NSKeyValueObservingProtocol
+import platform.Foundation.NSKeyValueObservingProtocol
 
 @OptIn(ExperimentalForeignApi::class)
 internal class VolumeReceiver(private val onVolumeChange: (Int) -> Unit) {
     private val session = AVAudioSession.sharedInstance()
-//    private var observer: VolumeObserver? = null
+    private var observer: VolumeObserver? = null
     private val volumeKey = "outputVolume"
 
-//    private inner class VolumeObserver : NSObject(), NSKeyValueObservingProtocol {
-//        override fun observeValueForKeyPath(
-//            keyPath: String?,
-//            ofObject: Any?,
-//            change: Map<Any?, *>?,
-//            context: COpaquePointer?
-//        ) {
-//            if (keyPath == volumeKey) {
-//                val volume = change?.get(NSKeyValueChangeNewKey) as? Float ?: return
-//                onVolumeChange((volume * 100).toInt())
-//            }
-//        }
-//    }
+    private inner class VolumeObserver : NSObject(), NSKeyValueObservingProtocol {
+        override fun observeValueForKeyPath(
+            keyPath: String?,
+            ofObject: Any?,
+            change: Map<Any?, *>?,
+            context: COpaquePointer?
+        ) {
+            if (keyPath == volumeKey) {
+                val volume = change?.get(NSKeyValueChangeNewKey) as? Float ?: return
+                onVolumeChange((volume * 100).toInt())
+            }
+        }
+    }
 
     init {
         session.setCategory(AVAudioSessionCategoryAmbient, error = null)
@@ -39,21 +39,21 @@ internal class VolumeReceiver(private val onVolumeChange: (Int) -> Unit) {
     }
 
     fun register() {
-//        val newObserver = VolumeObserver()
-//        observer = newObserver
-//        session.addObserver(
-//            observer = newObserver,
-//            forKeyPath = volumeKey,
-//            options = NSKeyValueObservingOptionNew,
-//            context = null
-//        )
+        val newObserver = VolumeObserver()
+        observer = newObserver
+        session.addObserver(
+            observer = newObserver,
+            forKeyPath = volumeKey,
+            options = NSKeyValueObservingOptionNew,
+            context = null
+        )
     }
 
     fun unregister() {
-//        observer?.let {
-//            session.removeObserver(it, volumeKey)
-//            observer = null
-//        }
+        observer?.let {
+            session.removeObserver(it, volumeKey)
+            observer = null
+        }
     }
 
     fun getCurrentVolume(): Int {
